29508c799cf12b9104d038d613c8ca54982c11ec
Felix Kaechele <felix@kaechele.ca>
2023-02-20 09:14:37 -0500

    feat: add __all__ exports
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- src/bonaparte/__init__.py
+++ src/bonaparte/__init__.py
@@ -1,1 +1,7 @@
+from __future__ import annotations
+
 __version__ = "0.1.0"
+
+from .fireplace import Fireplace, FireplaceFeatures, FireplaceState
+
+__all__ = ["Fireplace", "FireplaceFeatures", "FireplaceState"]
