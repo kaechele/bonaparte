f41cd64bd4b42050e27757911a30bb6f81fc05fe
Felix Kaechele <felix@kaechele.ca>
2023-09-06 20:07:05 -0400

    fix: sync version numbers
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- src/bonaparte/__init__.py
+++ src/bonaparte/__init__.py
@@ -1,7 +1,7 @@
 """The Bonaparte Library."""
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .fireplace import Fireplace, FireplaceFeatures, FireplaceState
 
