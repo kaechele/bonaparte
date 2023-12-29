504a907efeb35a9d6981f6cb8cb800b2f129fd03
Felix Kaechele <felix@kaechele.ca>
2023-12-29 18:23:09 +0100

    docs: automatically update project version upon release
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Bonaparte"
 copyright = "2023, Felix Kaechele"
 author = "Felix Kaechele"
-release = "0.1.0"
+release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
