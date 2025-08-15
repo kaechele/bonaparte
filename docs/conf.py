973e0b3ce0344d154044f28de957fcdaf79ded67
semantic-release <semantic-release>
2025-08-15 05:45:24 +0000

    1.0.1
    Automatically generated by python-semantic-release

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Bonaparte"
 copyright = "2023-2025, Felix Kaechele"
 author = "Felix Kaechele"
-release = "1.0.0"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/bonaparte/__init__.py
+++ src/bonaparte/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .fireplace import Fireplace, FireplaceFeatures, FireplaceState
 
